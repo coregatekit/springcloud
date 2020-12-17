ccdce445f3851d378e6e8264f35788f8bb74ce5a
Joe Kim <joesm.genie@gmail.com>
2020-12-17 16:54:50 +0700

    Initial gallery service

---
+++ gallery-service/src/main/java/coregatekit/springcloud/galleryservice/GalleryServiceApplication.java
@@ -0,0 +1,30 @@
+package coregatekit.springcloud.galleryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GalleryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryServiceApplication.class, args);
+	}
+
+}
+
+@Configuration
+class RestTemplateConfig {
+
+	// Create a bean restTemplate to call service
+	@Bean
+	@LoadBalanced							// Load balance between service instances runnign at different port
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
